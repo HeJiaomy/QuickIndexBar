3a8c2d3a3e118d913c26be213116d85df008ca58
HeJiaomy <121916986@qq.com>
2019-01-14 22:28:48 +0800

    custom quickIndexBar

---
+++ app/src/main/java/com/example/hj/quickindexbar/QuickIndexBar.java
@@ -0,0 +1,56 @@
+package com.example.hj.quickindexbar;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class QuickIndexBar extends View {
+
+    private String[] indexArr = {"A", "B", "C", "D", "E", "F", "G", "H",
+            "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
+            "V", "W", "X", "Y", "Z"};
+
+    private Paint paint;
+    private int width;
+
+    public QuickIndexBar(Context context) {
+        super(context);
+        init();
+    }
+
+    public QuickIndexBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public QuickIndexBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.WHITE);
+        paint.setTextSize(16);
+        paint.setTextAlign(Paint.Align.CENTER);//设置文本的起点是文字底边的中心
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        width = getMeasuredWidth();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int x = width / 2;
+//        for (String anIndexArr : indexArr) {
+            canvas.drawText("A", x, 30, paint);
+//        }
+
+    }
+}
