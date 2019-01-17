a12e01e1006a589c4f73d537da11a548b0506d4f
HeJiaomy <121916986@qq.com>
2019-01-17 22:33:37 +0800

    绘制文本的Y坐标

--- app/src/main/java/com/example/hj/quickindexbar/QuickIndexBar.java
+++ app/src/main/java/com/example/hj/quickindexbar/QuickIndexBar.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -15,6 +16,7 @@ public class QuickIndexBar extends View {
 
     private Paint paint;
     private int width;
+    private float cellHeight;
 
     public QuickIndexBar(Context context) {
         super(context);
@@ -34,7 +36,7 @@ public class QuickIndexBar extends View {
     private void init() {
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setColor(Color.WHITE);
-        paint.setTextSize(16);
+        paint.setTextSize(32);
         paint.setTextAlign(Paint.Align.CENTER);//设置文本的起点是文字底边的中心
     }
 
@@ -42,15 +44,23 @@ public class QuickIndexBar extends View {
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         width = getMeasuredWidth();
+        //得到一个格子的高度
+        cellHeight = getMeasuredHeight() / indexArr.length;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int x = width / 2;
-//        for (String anIndexArr : indexArr) {
-            canvas.drawText("A", x, 30, paint);
-//        }
+        for (int i = 0; i < indexArr.length; i++) {
+            int x = width / 2;
+            float y = cellHeight / 2 + getTextHeight(indexArr[i]) + i * cellHeight;
+            canvas.drawText(indexArr[i], x, y, paint);
+        }
+    }
 
+    private float getTextHeight(String str) {
+        Rect bounds = new Rect();
+        paint.getTextBounds(str, 0, str.length(), bounds);
+        return bounds.height();
     }
 }
